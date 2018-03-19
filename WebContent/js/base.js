77f93ac96ef12a64ea46aba91dfca0ad9e1ae24f
Nguyen tan Tho <nguyentantho1302@gmail.com>
2018-03-19 16:27:10 -0500

    Modify base js

--- WebContent/js/base.js
+++ WebContent/js/base.js
@@ -1,32 +1,22 @@
 $(function(){
-    var loading = $('#loadbar').hide();
-    $(document)
-    .ajaxStart(function () {
-        loading.show();
-    }).ajaxStop(function () {
-      loading.hide();
-    });
-    
-    $("#wrapper").toggleClass("toggled");
-    
-    $("label.btn").on('click',function () {
-      var choice = $(this).find('input:radio').val();
-      $('#loadbar').show();
-      $('#quiz').fadeOut();
-      setTimeout(function(){
-           $( "#answer" ).html(  $(this).checking(choice) );      
-            $('#quiz').show();
-            $('#loadbar').fadeOut();
-           /* something else */
-      }, 1500);
-    });
+  var loading = $('#loadbar').hide();
+  $(document)
+  .ajaxStart(function () {
+    loading.show();
+  }).ajaxStop(function () {
+    loading.hide();
+  });
 
-    $ans = 3;
+  $("label.btn").on('click',function () {
+    var choice = $(this).find('input:radio').val();
+    $('#loadbar').show();
+    $('#quiz').fadeOut();
+    setTimeout(function(){
+      $( "#answer" ).html(  $(this).checking(choice) );      
+      $('#quiz').show();
+      $('#loadbar').fadeOut();
+      /* something else */
+    }, 1500);
+  });
 
-    $.fn.checking = function(ck) {
-        if (ck != $ans)
-            return 'INCORRECT';
-        else 
-            return 'CORRECT';
-    }; 
 }); 
